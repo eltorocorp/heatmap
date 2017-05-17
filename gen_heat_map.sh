cb74f38ef514a3b323512461849c568a53d0dfd4
Justin Johnson <justin.johnson@eltoro.com>
2017-05-17 12:18:33 -0400

    fixed gen_heat_map to correct resolution

--- gen_heat_map.sh
+++ gen_heat_map.sh
@@ -1,3 +1,3 @@
 ./latlong2point
-cat points.txt | examples/heatmap_gen 500 500 > heatmap.png
+cat points.txt | examples/heatmap_gen 1920 1080 > heatmap.png
 sudo mv heatmap.png /var/www/html/images/heatmap.png
