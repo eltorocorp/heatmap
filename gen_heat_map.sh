048ce4982bc9c54d416c5f7d19b854223cf8c865
Ubuntu <ubuntu@ip-10-0-1-224.ec2.internal>
2017-05-19 20:37:10 +0000

    More changes to gen_heat_map.sh

--- gen_heat_map.sh
+++ gen_heat_map.sh
@@ -1,10 +1,8 @@
 ./latlong2point
 cat points.txt | examples/heatmap_gen 1920 1080 > heatmap.png
-sudo cp heatmap.png /var/www/html/images/heatmap.png
-cp data.txt data_files/starbucks_data.txt
-cp data_files/louisville_thrift_store_data.txt data.txt
+sudo yes | cp -rf heatmap.png /var/www/html/images/heatmap.png
+yes | cp -rf data_files/louisville_thrift_store_data.txt data.txt
 ./latlong2point
 cat points.txt | examples/heatmap_gen 1920 1080 > heatmap2.png
-sudo cp heatmap2.png /var/www/html/images/heatmap2.png
-cp data.txt data_files/louisville_thrift_store_data.txt
-cp data_files/starbucks_data.txt data.txt
+sudo yes | cp -rf heatmap2.png /var/www/html/images/heatmap2.png
+yes | cp -rf data_files/starbucks_data.txt data.txt
