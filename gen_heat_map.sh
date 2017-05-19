b8c579fae864f11da754948b9ea8cf658ab376ce
Ubuntu <ubuntu@ip-10-0-1-224.ec2.internal>
2017-05-19 19:37:34 +0000

    Hopefully, this fixes everything

--- gen_heat_map.sh
+++ gen_heat_map.sh
@@ -1,10 +1,10 @@
 ./latlong2point
 cat points.txt | examples/heatmap_gen 1920 1080 > heatmap.png
-sudo mv heatmap.png /var/www/html/images/heatmap.png
-mv data.txt data_files/starbucks_data.txt
-mv data_files/louisville_thrift_store_data.txt data.txt
+sudo cp heatmap.png /var/www/html/images/heatmap.png
+cp data.txt data_files/starbucks_data.txt
+cp data_files/louisville_thrift_store_data.txt data.txt
 ./latlong2point
 cat points.txt | examples/heatmap_gen 1920 1080 > heatmap2.png
-sudo mv heatmap2.png /var/www/html/images/heatmap2.png
-mv data.txt data_files/louisville_thrift_store_data.txt
-mv data_files/starbucks_data data.txt
+sudo cp heatmap2.png /var/www/html/images/heatmap2.png
+cp data.txt data_files/louisville_thrift_store_data.txt
+cp data_files/starbucks_data.txt data.txt
