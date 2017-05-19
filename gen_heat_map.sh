790171f36ea5629f38e4728424699c6b6f6829fc
Ubuntu <ubuntu@ip-10-0-1-224.ec2.internal>
2017-05-19 16:24:21 +0000

    gen_heat_map.sh now sets data files in their original position

--- gen_heat_map.sh
+++ gen_heat_map.sh
@@ -4,5 +4,7 @@ sudo mv heatmap.png /var/www/html/images/heatmap.png
 mv data.txt data_files/starbucks_data.txt
 mv data_files/louisville_thrift_store_data.txt data.txt
 ./latlong2point
-cat points.txt | example/heatmap_gen 1920 1080 > heatmap2.png
+cat points.txt | examples/heatmap_gen 1920 1080 > heatmap2.png
 sudo mv heatmap2.png /var/www/html/images/heatmap2.png
+mv data.txt data_files/louisville_thrift_store_data.txt
+mv data_files/starbucks_data data.txt
